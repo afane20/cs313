570650431f6e9f4495f12cb0282d49d26822f643
Ernesto Afane <afa12002@byui.edu>
2015-01-24 23:05:16 -0700

    Update and Assignment

---
+++ survey/assign03.php
@@ -0,0 +1,144 @@
+<?php
+	header("location: ")
+>
+
+<!doctype html>
+<html>
+<head>
+   <link rel="stylesheet" type="text/css" href="../index.css" />
+   <meta charset="UTF-8">
+   <title>Survey</title>
+</head>
+
+<body>
+
+<div id="wrapper">
+    <header id="top">
+        <h1>Survey</h1>
+        
+        <blockquote><p><b><i>
+        "Behold, God is my salvation; I will trust, and not be afraid; for the Lord Jehovah is my strength and my song;
+         he also has become my salvation".(2 Nephi 22:2)</i></b></p></blockquote>
+
+        
+      <nav id="mainnav">
+            <ul style="list-style-type:none; margin:0px;padding:0px">
+                <li><a href="../index.html">Home</a></li>
+                
+                <li><a href="http://www.byui.edu/">BYU-I</a></li>
+                
+                <li><a href="../assignments.html">CS213 Assignments</a></li> 
+                
+                <li><a href="https://byui.brainhoney.com/" >I-Learn</a></li>  
+                              
+                <li><a href="https://pod51034.outlook.com/">BYU-I email</a></li>
+            </ul>
+      </nav>
+    </header>
+    
+  <div>
+    <br/>
+    <br/>
+ </div>
+        
+<form id="form1" name="form1" method="post" action="survey1.php"">
+
+  <table width="688" height="400" border="1" align="center" cellpadding="6" tbcolor: bgcolor="#61E6B9">
+    <tbody>
+      <tr>
+      
+        <td width="365" style="text-align: right"><p>
+          <label for="Name">What operating system do you like more? </label>
+        </p></td>
+        
+        <td width="351" style="text-align: left">      
+       		<label>
+            <input type="radio" name="RadioGroup1" value="Windows" id="RadioGroup1_0">
+            Windows</label>
+            
+            <br>
+            <input type="radio" name="RadioGroup1" value="Apple" id="RadioGroup1_1" checked = "true">
+            OS X Yosemite (Apple)</label>
+            
+            <br>
+            <input type="radio" name="RadioGroup1" value="Linux" id="RadioGroup1_2">
+            Linux</label>
+            
+           
+        
+        
+        </td>
+      </tr>
+      <tr>
+        <td style="text-align: right"><p>
+          <label for="phone">What is your favorite video game?</label>
+        </p></td>
+        <td style="text-align: left"> <select name="game" id="bestGame">
+            <option value="League of Legends">League of Legends</option>
+            <option value="Legend of Zelda">Legend of Zelda</option>
+            <option value="Final Fantasy">Final Fantasy</option>
+            <option value="World of WarCraft">World of warCraft</option>
+          </select></td>
+      </tr>
+      
+      <tr>
+        <td style="text-align: right"><p>Who is your favorite teacher?</p></td>
+        <td style="text-align: left"><p>
+          <label>
+            <input type="radio" name="RadioGroup2" value="Ercanbrack" id="RadioGroup2_0" checked = "true">
+            Ercanbrack</label>
+          <br>
+          <label>
+            <input type="radio" name="RadioGroup2" value="Twitchell" id="RadioGroup2_1">
+            Twitchell</label>
+          <br>
+          <label>
+            <input type="radio" name="RadioGroup2" value="Helfrich" id="RadioGroup2_2">
+            Helfrich</label>
+          <br>
+    
+        </p></td>
+      </tr>
+      <tr>
+        <td style="text-align: right"><label for="food">What is your favorite food?</label></td>
+        <td style="text-align: left">
+          
+          <select name="food" id="favorite food">
+            <option value="Chinese">Chinese</option>
+            <option value="American">American</option>
+            <option value="Mexican">Mexican</option>
+            <option value="Italian">Italian</option>
+          
+          </select>
+        </td>
+      </tr>
+     
+      
+      
+      <tr>
+         <td style="text-align: right">
+           <input class="myButton" type="reset" name="reset" id="reset" value="Reset form">
+         </td>
+         <td><input class="myButton" type="submit" name="submit" value="Submit" >
+         </td>
+    </tbody>
+  </table>
+
+</form>
+    <br/>
+    <hr />
+    
+    <footer>
+      <p>&copy; Salvador Ernesto Afane</p> 
+      <h3><a href="#top">click here to Go to the Top</a></h3>
+
+   <p><a href="http://jigsaw.w3.org/css-validator/check/referer">
+    <img style="border:0; width:88px; height:31px"
+         src="http://jigsaw.w3.org/css-validator/images/vcss-blue"
+         alt="Valid CSS!" /></a>
+</p>
+    </footer>
+    
+</div>
+</body>
+</html>
---
+++ survey/survey1.php
@@ -0,0 +1,190 @@
+<?php
+	session_start();
+	$_SESSION["onlyOne"] = "survey";
+?>
+
+<!doctype html>
+<html>
+<head>
+   <link rel="stylesheet" type="text/css" href="../index.css" />
+   <meta charset="UTF-8">
+   <title>Ernesto Afane's Home Page</title>
+   
+   <script>
+   var _chart2_plot_properties;
+$(document).ready(function(){ 
+setTimeout( function() { 
+_chart2_plot_properties = {
+  "axes":{
+    "xaxis":{
+      "ticks":[
+        "a","b","c","d"
+      ],"renderer":$.jqplot.CategoryAxisRenderer,"properties":"xaxis"
+    }
+  },"animate":true,"animateReplot":true,"seriesDefaults":{
+    "renderer":$.jqplot.BarRenderer,"pointLabels":{
+      "show":true
+    }
+  }
+}
+
+
+
+$.jqplot.config.enablePlugins = true;
+$.jqplot.config.defaultHeight = 300;
+$.jqplot.config.defaultWidth  = 400;
+ _chart2= $.jqplot("chart2", [[2,6,7,10],[7,5,3,2]], _chart2_plot_properties);
+
+}, 200 );
+});
+   </script>
+</head>
+
+<body>
+
+<div id="wrapper">
+    <header id="top">
+        <h1>Survey</h1>
+        
+        <blockquote><p><b><i>
+        "Behold, God is my salvation; I will trust, and not be afraid; for the Lord Jehovah is my strength and my song;
+         he also has become my salvation".(2 Nephi 22:2)</i></b></p></blockquote>
+
+        
+      <nav id="mainnav">
+            <ul style="list-style-type:none; margin:0px;padding:0px">
+                <li><a href="../index.html">Home</a></li>
+                
+                <li><a href="http://www.byui.edu/">BYU-I</a></li>
+                
+                <li><a href="../assignments.html">CS213 Assignments</a></li> 
+                
+                <li><a href="https://byui.brainhoney.com/" >I-Learn</a></li>  
+                              
+                <li><a href="https://pod51034.outlook.com/">BYU-I email</a></li>
+            </ul>
+      </nav>
+    </header>
+    
+  <div>
+       <br>
+        <?php 
+        
+       //  //Testin to read a file and add a <td> to the table, adding values instead. PASSED
+//         $readFile = "data.txt";
+//        	$document = file_get_contents($readFile);
+//        	print("$document");
+//         
+        
+        //Example array.
+	//Retrieve the data from our text file.
+	$fileContents = file_get_contents('json_array.txt');
+
+	//Convert the JSON string back into an array.
+	$decoded = json_decode($fileContents, true);
+
+	//The end result.
+	//var_dump($decoded);
+    
+        
+        $OS = $_POST["RadioGroup1"];
+    	$game = $_POST["game"];
+    	$teacher = $_POST["RadioGroup2"];
+    	$food = $_POST["food"];
+    	
+    	$OS1 = 0;
+    	$OS2 = 0;
+    	$OS3 = 0;
+    	
+    	$game1 = 0;
+    	$game2 = 0;
+    	$game3 = 0;
+    	$game4 = 0;
+
+    	$teacher1 = 0;
+    	$teacher2 = 0;
+    	$teacher3 = 0;
+    	
+    	$food1 = 0;
+    	$food2 = 0;
+    	$food3 = 0;
+    	$food4 = 0;
+    	
+    	$OS1 = $decoded[4];
+		$OS2 = $decoded[5];
+		$OS3 = $decoded[6];
+		
+		$game1 = $decoded[7];
+		$game2 = $decoded[8];
+		$game3 = $decoded[9];
+		$game4 = $decoded[10];
+		
+		$teacher1 = $decoded[11];  
+    	$teacher2 = $decoded[12];  
+    	$teacher3 = $decoded[13];
+    	
+    	$food1 = $decoded[14]; 
+    	$food2 = $decoded[15];
+    	$food3 = $decoded[16];
+    	$food4 = $decoded[17];
+    	
+    	if ($OS == "Windows"){$OS1++;}
+    	if ($OS == "Apple"){$OS2++;}
+    	if ($OS == "Linux"){$OS3++;}
+    	if ($game == "League of Legends"){$game1++;}
+    	if ($game == "Legend of Zelda"){$game2++;}
+    	if ($game == "Final Fantasy"){$game3++;}
+    	if ($game == "World of WarCraft"){$game4++;}
+    	if ($teacher == "Ercanbrack"){$teacher1++;}
+    	if ($teacher == "Twitchell"){$teacher2++;}
+    	if ($teacher == "Helfrich"){$teacher3++;}
+    	if ($food == "Chinese"){$food1++;}
+    	if ($food == "American"){$food2++;}
+    	if ($food == "Mexican"){$food3++;}
+    	if ($food == "Italian"){$food4++;}
+    	
+    	//starts with 4 OS, game at 7, teacher at 11, food at 14
+    	$info = array($OS, $game, $teacher, $food, $OS1, 
+        			  $OS2 , $OS3 , $game1, $game2, $game3, $game4, $teacher1, $teacher2, $teacher3, 
+        			  $food1, $food2, $food3, $food4);
+        			  
+    //Encode the array into a JSON string.
+    $encodedString = json_encode($info);
+
+	//Save the JSON string to a text file.
+	file_put_contents('json_array.txt', $encodedString);
+    ?>
+       
+  </div>
+ <div id = "survey">
+ <?php
+ 	
+ 	echo '<h2>'."Favorite Operating System".'</h2><h3>';
+ 	
+ 	$html = 'Windows '.$info[4].'<br>'.'Apple '.$info[5].'<br>'.'Linux '.$info[6].'<br></h3>';
+ 	echo "$html";
+ 	
+ 		echo '<h2>'."Favorite Video Game".'</h2><h3>';
+ 	
+ 	$html = 'League of Legends '.$info[7].'<br>'.'Legend of Zelda '.$info[8].'<br>'.'Final Fantasy '.$info[9].'<br>'.
+ 	'World of WarCraft '.$info[10].'<br></h3>';
+ 	echo "$html";
+ 	
+ 		echo '<h2>'."Favorite teacher".'</h2><h3>';
+ 	
+ 	$html = 'Ercanbrack '.$info[11].'<br>'.'Twitchell '.$info[12].'<br>'.'Helfrich '.$info[13].'<br></h3>';
+ 	 echo "$html";
+
+ 	
+ 	echo '<h2>'."Favorite Food".'</h2><h3>';
+ 	
+ 	$html = 'Chinese '.$info[14].'<br>'.'American '.$info[15].'<br>'.'Mexican '.$info[16].'<br>'.'Italian '.$info[17].'<br></h3>';
+ 	 echo "$html";
+
+ ?>
+ </div>
+ </div>
+ </body>
+ </html>
+ 
+ 

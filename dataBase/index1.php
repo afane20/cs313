58f81ecd6ff3db5b16c2050597c908f499f19c53
Ernesto Afane <afa12002@byui.edu>
2015-02-07 07:37:21 -0700

    DataBase Start!

---
+++ dataBase/index1.php
@@ -0,0 +1,24 @@
+<?php
+if(!$_SESSION){
+    session_start();
+ }
+require $_SERVER['DOCUMENT_ROOT'].'/practice2.php';
+if ($_POST['action'] == "Search" && !empty($_POST['search'])){
+	$searchVariable = $_POST['search'];
+	$scriptures = getSpecificScriptures($searchVariable);
+	$output = "<h1>Scripture Resources</h1>";
+	foreach ($scriptures as $value) {
+		$output .= "<b>$value[1] $value[2]:$value[3]</b> - <br /><br />";
+	}
+	include "view.php";
+	exit;
+}
+else {
+	$scriptures = getScriptures();
+	$output = "<h1>Scripture Resources</h1>";
+	foreach ($scriptures as $value) {
+		$output .= "<b>$value[1] $value[2]:$value[3]</b> - <br /><br />";
+	}
+	include "view.php";
+	exit;
+}

1d49561b9658f579392cdf506ee69cc3d47dfe98
afane20 <salvadormcintosh@hotmail.com>
2016-01-08 17:05:41 -0500

    Update to the database
    For the new server, ashlie email

--- dataBase/2fit/connect.php
+++ dataBase/2fit/connect.php
@@ -1,9 +1,9 @@
 <?php
-$connection = mysql_connect('127.7.209.2', 'afane', 'rareriroru');
+$connection = mysql_connect('127.11.59.130 ', 'afane', 'rareriroru');
 if (!$connection){
     die("Database Connection Failed" . mysql_error());
 }
-$select_db = mysql_select_db('Fitness');
+$select_db = mysql_select_db('2Fit');
 if (!$select_db){
     die("Database Selection Failed" . mysql_error());
 }

a6d6c4598a708b409378b5625975d753fc45382e
afane20 <salvadormcintosh@hotmail.com>
2017-01-05 01:15:37 -0500

    Update connect.php

--- dataBase/2fit/connect.php
+++ dataBase/2fit/connect.php
@@ -1,9 +1,9 @@
 <?php
-$connection = mysql_connect('127.11.59.130', 'afane', 'rareriroru');
+$connection = mysql_connect('127.3.147.2', 'afane', 'rareriroru');
 if (!$connection){
     die("Database Connection Failed" . mysql_error());
 }
-$select_db = mysql_select_db('2Fit');
+$select_db = mysql_select_db('2fitness');
 if (!$select_db){
     die("Database Selection Failed" . mysql_error());
 }
